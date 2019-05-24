28f001c89a7e2953a4655045adf2e6ca724549a1
yurikrupnik <krupnik.yuri@gmail.com>
2019-05-25 01:43:03 +0300

    init

--- rollup.config.js
+++ rollup.config.js
@@ -2,18 +2,18 @@ import babel from 'rollup-plugin-babel';
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 // import { uglify } from 'rollup-plugin-uglify';
-import { terser } from 'rollup-plugin-terser';
+// import { terser } from 'rollup-plugin-terser';
 import path from 'path';
-import reduce from 'lodash/reduce'; // eslint-disable-line
+// import reduce from 'lodash/reduce'; // eslint-disable-line
 import autoExternal from 'rollup-plugin-auto-external';
-import pkg from './package.json';
+// import pkg from './package.json';
 
 const name = process.cwd();
 const input = path.join(name, 'app/index.js');
-const filter = reduce(
-    Object.assign({}, pkg.peerDependencies, pkg.dependencies),
-    (acc, val, key) => acc.concat(key), []
-);
+// const filter = reduce(
+//     Object.assign({}, pkg.peerDependencies, pkg.dependencies),
+//     (acc, val, key) => acc.concat(key), []
+// );
 
 const fileName = 'index.js';
 const plugins = [
@@ -34,6 +34,6 @@ export default {
         file: path.join(name, 'dist', `${fileName}`),
         format: 'cjs'
     },
-    external: filter,
+    // external: filter,
     plugins
 };
---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const nodeExternals = require('webpack-node-externals');
+const CopyPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+    context: path.resolve(process.cwd(), 'app'),
+    target: 'node',
+    // node: {
+    //     __dirname: false,
+    //     __filename: true,
+    // },
+    externals: [nodeExternals()], // in order to ignore all modules in node_modules folder
+    entry: './index.js',
+    output: {
+        path: path.resolve(process.cwd(), 'dist'),
+        chunkFilename: '[name].js'
+    },
+    mode: 'production',
+    module: {
+        rules: [
+            {
+                test: /\.(js)$/,
+                use: ['babel-loader', 'eslint-loader'],
+            }
+        ]
+    },
+    plugins: [
+        new CopyPlugin([
+            { from: 'templates', to: 'templates' }
+        ]),
+    ]
+};

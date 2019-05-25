ec6deb05334fd167dd2b000ab3d8f73d3dc74626
yurikrupnik <krupnik.yuri@gmail.com>
2019-05-25 21:56:11 +0300

    init

--- packages/generator-node-puzzle-babel/app/__tests__/babelGenerator.js
+++ packages/generator-node-puzzle-babel/app/__tests__/babelGenerator.js
@@ -8,37 +8,34 @@ const {
 } = global;
 
 describe('Babel Generator', () => {
-    test('babelrc file', function () {
-        return helpers.run(path.join(__dirname, '../index.js'))
-            .withOptions({
-                react: false
-            })
-            .then(function () {
-                assert.file('.babelrc');
-                assert.noFileContent('.babelrc', 'react-loadable/babel');
-                assert.noFileContent('.babelrc', '@babel/preset-react');
-            });
-    });
+    test('babelrc file', () => helpers
+        .run(path.join(__dirname, '../index.js'))
+        .withOptions({
+            react: false
+        })
+        .then(() => {
+            assert.file('.babelrc');
+            assert.noFileContent('.babelrc', 'react-loadable/babel');
+            assert.noFileContent('.babelrc', '@babel/preset-react');
+        }));
 
-    test('react option', function () {
-        return helpers.run(path.join(__dirname, '../index.js'))
-            .withOptions({
-                react: true,
-            })
-            .then(function () {
-                assert.file('.babelrc');
-                assert.fileContent('.babelrc', 'react-loadable/babel');
-                assert.fileContent('.babelrc', '@babel/preset-react');
-            });
-    });
+    test('react option', () => helpers
+        .run(path.join(__dirname, '../index.js'))
+        .withOptions({
+            react: true,
+        })
+        .then(() => {
+            assert.file('.babelrc');
+            assert.fileContent('.babelrc', 'react-loadable/babel');
+            assert.fileContent('.babelrc', '@babel/preset-react');
+        }));
 
-    test('destination option', () => {
-        return helpers.run(path.join(__dirname, '../index.js'))
-            .withOptions({
-                destination: 'rand'
-            })
-            .then(function () {
-                assert.file('rand/.babelrc');
-            });
-    });
+    test('destination option', () => helpers
+        .run(path.join(__dirname, '../index.js'))
+        .withOptions({
+            destination: 'rand'
+        })
+        .then(() => {
+            assert.file('rand/.babelrc');
+        }));
 });

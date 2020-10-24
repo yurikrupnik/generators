b10793f902b26994bea9bf4f7bb5eee8307687b6
yurikrupnik <krupnik.yuri@gmail.com>
2020-10-24 15:19:53 +0300

    remove publish from git hub actions and circleci

---
+++ scripts/npm-publish.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+set -eo pipefail
+
+npx lerna publish patch --no-push --yes --conventional-commits
+npx lerna exec --stream --since -- npm i
+npx lerna exec -- npm install --package-lock-only --ignore-scripts --no-audit
+git add -u
+git diff --quiet && git diff --staged --quiet || git commit -am "package-lock.json update"
+git push origin --follow-tags
